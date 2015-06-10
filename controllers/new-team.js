271374ee8ed7d092267d170d28de84704ecf8a0d
Cory Spitzer & jordan <student@epicodus.com>
2015-06-10 16:08:04 -0700

    add two teams at a time

--- controllers/new-team.js
+++ controllers/new-team.js
@@ -1,12 +1,16 @@
 Stats.NewTeamController = Ember.Controller.extend({
   actions: {
-    saveTeam: function() {
-      var newTeam = this.store.createRecord('team', {
-        teamName: this.get('teamName')
+    saveTeams: function() {
+      var newTeam1 = this.store.createRecord('team', {
+        teamName: this.get('teamName1')
+      });
+      var newTeam2 = this.store.createRecord('team', {
+        teamName: this.get('teamName2')
       });
 
-      newTeam.save();
-
+      newTeam1.save();
+      newTeam2.save();
+      this.transitionToRoute('teams');
     }
   }
 });
